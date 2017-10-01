286688f502ac4705fdb7984f8b083d6a07c6d26a
scott-wyatt <scottwyatt86@gmail.com>
2017-10-01 19:02:38 -0400

    remove misplaced test

--- test/unit/services/CountryService.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-/* global describe, it */
-const assert = require('assert')
-
-describe('CountryService', () => {
-  it('should exist', () => {
-    assert(global.app.api.services['CountryService'])
-  })
-})
